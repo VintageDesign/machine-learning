297d70c4f139e1479414ffc30b32c3fbfa060be1
Riley Kopp <koppril7@gmail.com>
2020-09-20 20:44:11 -0600

    Moved the perceptron to the ML file

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from perceptron import Perceptron
+from ML import Perceptron
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
