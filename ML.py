be74fc59a79c87d7d48efa684da99fb8542d00e1
Riley Kopp <koppril7@gmail.com>
2020-09-21 09:01:43 -0600

    I guess ML.py was never commited?

---
+++ ML.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+
+class Perceptron:
+    def __init__(self, scale, max_epochs):
+        self._scale = scale
+        self._max_epochs = max_epochs
+        self._weights = []
+
+    def _predict(self, x):
+        summation = self.net_input(x)
+        summation = np.where(summation > 0, 1, -1)
+        return summation
+
+    def net_input(self, x):
+        return np.dot(x, self._weights[1:]) + self._weights[0]
+
+    def fit(self, x, y):
+        self._weights = [0 for index in range(len(x[0])+1)]
+
+        for epoch in range(self._max_epochs):
+            error_rate = 0
+
+            for index in range(len(x)):
+                point = x[index]
+                prediction = self._predict(point)
+                error = y[index] - prediction
+                error_rate += error ** 2
+                self._weights[0] += self._scale * error
+                self._weights[1:] += self._scale * error * point
+            # print(error_rate)
+            if error_rate == 0:
+                break
+
+    def get_weights(self):
+        return self._weights
+
+    def predict(self, x):
+        retval = self._predict(x)
+        return retval
+
