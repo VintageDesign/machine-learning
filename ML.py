0714f69683b4445a39d2e2771f4efe4b353a2b93
Riley Kopp <koppril7@gmail.com>
2020-10-23 11:08:50 -0600

    Added Linear Regression and moved example code into examples.py

--- ML.py
+++ ML.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
 
 class Perceptron:
@@ -39,3 +41,99 @@ class Perceptron:
         retval = self._predict(x)
         return retval
 
+    def plot_decision_regions(self, X, y, classifier, resolution=0.02):
+        # setup marker generator and color map
+        shape = X.shape
+        if shape[1] == 2:
+            markers = ('s', 'x', 'o', '^', 'v')
+            colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+            cmap = ListedColormap(colors[:len(np.unique(y))])
+            # plot the decision surface
+            x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+            x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+            xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                                   np.arange(x2_min, x2_max, resolution))
+            Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+            Z = Z.reshape(xx1.shape)
+            plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+            plt.xlim(xx1.min(), xx1.max())
+            plt.ylim(xx2.min(), xx2.max())
+            # plot class samples
+            for idx, cl in enumerate(np.unique(y)):
+                plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
+                            alpha=0.8, c=cmap(idx),
+                            marker=markers[idx], label=cl)
+
+        elif shape[1] == 3:
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+            colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+            cmap = ListedColormap(colors[:len(np.unique(y))])
+            # plot the decision surface
+            x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+            x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+            x3_min, x3_max = X[:, 2].min() - 2, X[:, 2].max() + 1
+            xx1, xx2, xx3 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                                        np.arange(x2_min, x2_max, resolution),
+                                        np.arange(x3_min, x3_max, resolution))
+            l1, l2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                                 np.arange(x2_min, x2_max, resolution))
+            # Z = classifier.net_input(np.array([xx1.ravel(), xx2.ravel(), xx3.ravel()]).T)
+            w = classifier.get_weights()
+            Z = -1 * (w[0] + w[1] * l1.ravel() + w[2] * l2.ravel()) / w[3]
+            Z.reshape(l1.shape)
+            print(Z.shape)
+            ax.plot(l1.ravel(), l2.ravel(), Z)
+            ax.set_xlim(xx1.min(), xx1.max())
+            ax.set_ylim(xx2.min(), xx2.max())
+            ax.set_zlim(xx3.min(), xx3.max())
+            ax.scatter(X[:50, 0], X[:50, 1], X[:50, 2], color='red', marker='o', label='setosa')
+            ax.scatter(X[50:100, 0], X[50:100, 1], X[50:100, 2], color='blue', marker='x', label='versicolor')
+            ax.legend()
+
+
+class LinearRegression:
+    def __init__(self):
+        self._slope = 0
+        self._intercept = 0
+
+    def _get_mean(self, arr):
+        """
+        Calculates the mean of the array given
+
+        :param arr: The given array
+        :return: Mean
+        """
+
+        return np.mean(arr)
+
+    def fit(self, x, y):
+        """
+        Fits a linear model using least squares.
+
+        :param x: The list of independent variables
+        :param y: The list of dependent variables
+        :return: bool success
+        """
+        if len(x) != len(y):
+            print("Error: input list sizes must agree.")
+            raise AttributeError
+        x_mean = self._get_mean(x)
+        y_mean = self._get_mean(y)
+
+        top = 0
+        bottom = 0
+        for index in range(len(x)):
+            top += (x[index] - x_mean) * (y[index] - y_mean)
+            bottom += (x[index] - x_mean) ** 2
+
+        self._slope = top / bottom
+        self._intercept = y_mean - (self._slope * x_mean)
+
+        return True
+
+    def get_slope(self):
+        return self._slope
+
+    def get_intercept(self):
+        return self._intercept
---
+++ examples.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from ML import Perceptron, LinearRegression
+
+def perceptron_example():
+    df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
+
+    y = df.iloc[0:100, 4].values
+    y = np.where(y == 'Iris-setosa', -1, 1)
+
+    x = df.iloc[0:100, [0, 2]].values
+    xlabel_text = "Index 0"
+    ylabel_text = "Index 2"
+    zlabel_text = "Index 3"
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(x[:50, 0], x[:50, 1], color='red', marker='o', label='setosa')
+    ax.scatter(x[50:100, 0], x[50:100, 1], color='blue', marker='x', label='versicolor')
+    ax.set_xlabel(xlabel_text)
+    ax.set_ylabel(ylabel_text)
+    ax.legend()
+
+    perceptron = Perceptron(.1, 100)
+    perceptron.fit(x, y)
+    # print(perceptron.get_weights())
+    plt.figure()
+    perceptron.plot_decision_regions(x, y, perceptron)
+    plt.xlabel(xlabel_text)
+    plt.ylabel(ylabel_text)
+    plt.show()
+
+
+def linear_regression_example():
+    df = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/diamonds.csv')
+    df = df[df['price'] > 1]
+    x = df.iloc[1:, 6].values
+    y = df.iloc[1:, 0].values
+    print(y[0])
+
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(x[:], y[:], color='blue', marker='o', label='diamonds')
+    ax.legend()
+
+    regession = LinearRegression()
+
+    regession.fit(x, y)
+
+    fit_x = np.linspace(np.min(x), np.max(x), 100)
+    fit_y = regession.get_slope() * fit_x + regession.get_intercept()
+    ax.plot(fit_x, fit_y)
+    plt.show()
